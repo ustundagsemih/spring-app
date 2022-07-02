8536af3356b84b9408d416887cf3d6d6a28e36e4
ustundagsemih <ustundagsemih@gmail.com>
2022-07-03 02:57:59 +0300

    Jenkinsfile

--- src/main/java/com/ustundagsemih/app/APIController.java
+++ src/main/java/com/ustundagsemih/app/APIController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RestController
 public class APIController {
 
-	@GetMapping("/api")
+	@GetMapping("/api1")
 	public String index(HttpServletRequest request) {
         String test = request.getQueryString();
         System.out.println(test);
